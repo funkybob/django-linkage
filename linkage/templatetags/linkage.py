e4f77a4a6737d4e3478675f6bd0eea097804ffc5
Curtis <curtis@tinbrain.net>
2014-02-28 21:27:13 +1100

    Add link tag

--- linkage/templatetags/linkage.py
+++ linkage/templatetags/linkage.py
@@ -1,4 +1,6 @@
 
+from contextlib import contextmanager
+
 from linkage import models
 
 from django.db.models import Q
@@ -22,3 +24,26 @@ def load_links_with(*tags):
     for tag in tags:
         qs = qs.filter(tags__name=tag)
     return qs
+
+@contextmanager
+def extra_context(context, extra):
+    '''Temporarily add some context, and clean up after ourselves.'''
+    context.update(extra)
+    yield
+    context.pop()
+
+@register.simple_tag(takes_context=True)
+def link(context, slug, *args, **kwargs):
+    '''Embed a link'''
+    # Find the link by slug
+    try:
+        link = Link.objects.get(slug=slug)
+    except Link.DoesNotExist:
+        return ''
+
+    if 'template' in kwargs:
+        tmpl = get_template(kwargs.pop('template'))
+    else:
+        tmpl = template.Template('<a href="{{ link.url }}>{{ link.title }}</a>')
+    with extra_context(context, kwargs, link=link):
+        return tmpl.render(context)
