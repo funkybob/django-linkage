20bb512cb9a43e6ca550db1a3779aa8ea7efa6eb
funkybob <curtis@tinbrain.net>
2014-03-02 21:18:57 +1100

    Add menu tag

--- linkage/templatetags/linkage.py
+++ linkage/templatetags/linkage.py
@@ -6,11 +6,13 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def get_menu(name):
     return models.Menu.objects.select_related('items').get(
         Q(title=name) | Q(slug=name)
     )
+
 
 @register.assignment_tag
 def load_links_with(*tags):
@@ -22,3 +24,12 @@ def load_links_with(*tags):
     for tag in tags:
         qs = qs.filter(tags__name=tag)
     return qs
+
+
+@register.inclusion_tag(template_name="linkage/menu.html")
+def menu(menu_name):
+    menu = models.Menu.objects.select_related('items').get(
+        Q(title=name) | Q(slug=name)
+    )
+    return {'menu': menu}
+
