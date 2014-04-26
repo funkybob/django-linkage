0f0cae603dde7de15ea6192ec12c643c48d0cb84
Curtis <curtis@tinbrain.net>
2014-04-26 12:37:18 +1000

    Rework to product querysets

--- linkage/forms.py
+++ linkage/forms.py
@@ -1,24 +1,34 @@
 
+from django.db.models import get_models
 from django.contrib.contenttypes.models import ContentType
 from django import forms
 
 from . import models
 
 # Models we can find a list page for
-LISTABLE_OBJECTS = ContentType.objects.get_for_models(*[
-    model
-    for app, model in models.get_models()
-    if callable(getattr(model, 'get_list_url', None))
-])
 
+LISTABLE_OBJECTS = ContentType.objects.filter(
+    pk__in=[
+        v.pk
+        for v in ContentType.objects.get_for_models(*[
+            model
+            for model in get_models()
+            if callable(getattr(model, 'get_list_url', None))
+        ]).values()
+    ]
+)
 
 # The list of models we know how to get a url for
-LINKABLE_OBJECTS = ContentType.objects.get_for_models(*[
-    model
-    for app, model in models.get_models()
-    if callable(getattr(model, 'get_absolute_url', None))
-])
-
+LINKABLE_OBJECTS = ContentType.objects.filter(
+    pk__in=[
+        v.pk
+        for v in ContentType.objects.get_for_models(*[
+            model
+            for model in get_models()
+            if callable(getattr(model, 'get_absolute_url', None))
+        ]).values()
+    ]
+)
 
 class ObjectTypeLinkForm(forms.ModelForm):
 
